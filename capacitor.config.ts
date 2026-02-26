c3f9f37267cac381c8f779dffb6eaca5e73b0557
PRESTEBAN <estebanpesantez22@gmail.com>
2026-02-26 18:41:50 -0500

    cambios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'VentasIF',
-  webDir: 'www'
+  webDir: 'www',
+   server: {
+    allowNavigation: ['ventasif-if-api.onrender.com']
+  }
 };
 
 export default config;
